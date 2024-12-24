c8ff5bbfe5362185fa32b4efc8c2499a06813f09
LunaFreyjaNox <56685367+LunaFreyjaNox@users.noreply.github.com>
2024-12-24 22:44:09 +0100

    clean tags

--- kubejs/server_scripts/GTCEU/electronics_tags.js
+++ kubejs/server_scripts/GTCEU/electronics_tags.js
@@ -1,18 +1,7 @@
 ServerEvents.tags('item', event => {
-//MAX
-event.add('gtceu:circuits/ulv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/lv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/mv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/hv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/ev','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/iv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/luv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/zpm','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/uv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/uhv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/uev','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/uiv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/uxv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/opv','qilby_core:shushu_circuit_max');
-event.add('gtceu:circuits/max','qilby_core:shushu_circuit_max');
+
+        const tags = ['lv', 'mv', 'hv','iv','luv','zpm','uv','uhv','uev','uiv','uxv','opv','max']
+        tags.foreach(tag => {
+            event.add(`gtceu:circuits/${tag}`, 'qilby_core:shushu_circuit_max');
+        });
 });

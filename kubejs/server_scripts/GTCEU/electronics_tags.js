7a7c21eb304a96c0568bf373dcf7a61dafcedaa0
LunaFreyjaNox <56685367+LunaFreyjaNox@users.noreply.github.com>
2024-12-24 22:49:49 +0100

    fix typo

--- kubejs/server_scripts/GTCEU/electronics_tags.js
+++ kubejs/server_scripts/GTCEU/electronics_tags.js
@@ -1,7 +1,7 @@
 ServerEvents.tags('item', event => {
 
         const tags = ['lv', 'mv', 'hv','iv','luv','zpm','uv','uhv','uev','uiv','uxv','opv','max']
-        tags.foreach(tag => {
+        tags.forEach(tag => {
             event.add(`gtceu:circuits/${tag}`, 'qilby_core:shushu_circuit_max');
         });
 });

74c0f8089de90e6911e7166de1afacce7e5c5a8a
LunaFreyjaNox <56685367+LunaFreyjaNox@users.noreply.github.com>
2025-02-20 18:28:47 +0100

    add optical circuit recipe
    + optical assembler

---
+++ kubejs/server_scripts/GTCEU/Electronics/optical_circuit.js
@@ -0,0 +1,84 @@
+ServerEvents.recipes(event => {
+
+    event.recipes.gtceu.circuit_assembler('optical_processor')
+        .itemInputs(
+            'qilby_core:rushu_printed_circuit_board',
+            'qilby_core:optical_coputation_circuit',
+            '8x qilby_core:perfect_optic_fiber',
+            '4x qilby_core:perfect_nano_merger',
+            '8x qilby_core:hi_pulse_converter',
+            '4x qilby_core:perfect_nano_splitter'
+        )
+        .itemOutputs(
+            '12x qilby_core:optical_processor'
+        )
+        .duration(150)
+        .EUt(GTValues.VA[GTValues.UV])
+        .cleanroom(CleanroomType.CLEANROOM);
+
+    event.recipes.gtceu.circuit_assembler('optical_processor_assembly')
+        .itemInputs(
+            'qilby_core:rushu_printed_circuit_board',
+            '4x qilby_core:optical_processor',
+            '8x qilby_core:perfect_optic_fiber',
+            '4x qilby_core:perfect_nano_merger',
+            '8x qilby_core:hi_pulse_converter',
+            '4x qilby_core:perfect_nano_splitter'
+        )
+        .itemOutputs(
+            '6x qilby_core:optical_processor_assembly'
+        )
+        .duration(600)
+        .EUt(GTValues.VA[GTValues.UHV])
+        .cleanroom(CleanroomType.CLEANROOM);
+
+    event.recipes.gtceu.assembly_line('optical_processor_supercompter')
+        .itemInputs(
+            'qilby_core:rushu_printed_circuit_board',
+            '4x qilby_core:optical_processor_assembly',
+            '16x qilby_core:perfect_optic_fiber',
+            '8x qilby_core:perfect_nano_merger',
+            '16x qilby_core:hi_pulse_converter',
+            '8x qilby_core:perfect_nano_splitter'
+        )
+        .inputFluids(
+            Fluid.of('gtceu:soldering_alloy', 1152),
+            Fluid.of('gtceu:polybenzimidazole', 576),
+            Fluid.of('qilby_core:pure_glass', 1152)
+        )
+        .itemOutputs(
+            '3x qilby_core:optical_processor_computer'
+        )
+        .stationResearch(b => b.researchStack(Item.of('qilby_core:optical_processor_assembly')).CWUt(2048).EUt(GTValues.VA[GTValues.UHV]))
+        .duration(600)
+        .EUt(GTValues.VA[GTValues.UHV]);
+
+    event.recipes.gtceu.assembly_line('optical_processor_mainframe')
+        .itemInputs(
+            'gtceu:neutronium_frame',
+            'qilby_core:rushu_printed_circuit_board',
+            '2x qilby_core:optical_processor_computer',
+            '64x qilby_core:perfect_optic_fiber',
+            '32x qilby_core:perfect_nano_merger',
+            '64x qilby_core:hi_pulse_converter',
+            '32x qilby_core:perfect_nano_splitter',
+            '64x gtceu:polybenzimidazole_foil',
+            '64x gtceu:polybenzimidazole_foil',
+            '64x gtceu:ram_chip',
+            '16x gtceu:ruthenium_trinium_americium_neutronate_double_wire',
+            '8x gtceu:trinium_plate'
+
+        )
+        .inputFluids(
+            Fluid.of('gtceu:soldering_alloy', 2880),
+            Fluid.of('gtceu:polybenzimidazole', 1440),
+            Fluid.of('qilby_core:pure_glass', 1152)
+        )
+        .itemOutputs(
+            'qilby_core:optical_processor_mainframe'
+        )
+        .stationResearch(b => b.researchStack(Item.of('qilby_core:optical_processor_computer')).CWUt(4096).EUt(GTValues.VA[GTValues.UEV]))
+        .duration(600)
+        .EUt(GTValues.VA[GTValues.UEV]);
+
+});
---
+++ kubejs/startup_scripts/GTCEU/optical_assembler.js
@@ -0,0 +1,53 @@
+// priority: 3
+GTCEuStartupEvents.registry("gtceu:machine", (event) => {
+    event
+      .create("optical_assembly_line", "multiblock")
+      .rotationState(RotationState.NON_Y_AXIS)
+      .recipeType("optical_assembler")
+      .appearanceBlock(GTBlocks.ADVANCED_COMPUTER_CASING)
+      .recipeModifier(
+        GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(
+          OverclockingLogic.PERFECT_OVERCLOCK
+        )
+      )
+      .pattern((definition) => 
+        FactoryBlockPattern.start()
+      .aisle("  AAA  ", "  A@A  ", "  AAA  ", "  AAA  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("  AAA  ", "  BCB  ", "  ADA  ", "  EAE  ") 
+      .aisle("AAAAAAA", "AAA AAA", "AAA AAA", "AAAAAAA") 
+      .aisle("AAAAAAA", "A     A", "A     A", "AFFFFFA") 
+      .aisle("AAAAAAA", "G     G", "A     A", "AFFFFFA") 
+      .aisle("AAAAAAA", "A     A", "A     A", "AFFFFFA") 
+      .aisle("AAAAAAA", "AAAGAAA", "AAAAAAA", "AAAAAAA") 
+          .where("@", Predicates.controller(Predicates.blocks(definition.get())))
+          .where(
+            "A",
+            Predicates.blocks("gtnn:field_restriction_casing")
+              .setMinGlobalLimited(1)
+              .or(Predicates.abilities(PartAbility.COMPUTATION_DATA_RECEPTION)
+                    .setExactLimit(1)
+                )
+              .or(Predicates.autoAbilities(definition.getRecipeTypes()))
+          )
+          .where("B", Predicates.blocks("gtceu:fusion_glass"))
+          .where("C", Predicates.blocks("gtnn:high_speed_pipe_block"))
+          .where("D", Predicates.blocks("gtceu:assembly_line_unit"))
+          .where("E", Predicates.blocks("gtceu:assembly_line_grating"))
+          .where("F", Predicates.blocks("minecraft:tinted_glass"))
+          .where("G", Predicates.blocks("gtceu:uhv_256a_laser_source_hatch"))
+          .where(" ", Predicates.any())
+          .build()
+        )
+        .workableCasingRenderer(
+        "gtceu:block/casings/hpca/advanced_computer_casing/side",
+        "gtceu:block/multiblock/hpca",
+        true
+      )
+  });
+

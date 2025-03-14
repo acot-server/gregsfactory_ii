74bf1f2bf17d3a99826a4e9be5ace8ac237b4c36
Annwan <annwan@annwan.me>
2025-03-14 23:11:34 +0100

    add railcraft

--- kubejs/startup_scripts/OreCleanup.js
+++ kubejs/startup_scripts/OreCleanup.js
@@ -7,11 +7,26 @@ WorldgenEvents.remove( event => {
             // Create
             'create:zinc_ore',
             'create:deepslate_zinc_ore',
+            // Draconic
             'draconicevolution:overworld_draconium_ore',
             'draconicevolution:deepslate_draconium_ore',
             'draconicevolution:nether_draconium_ore',
-            'draconicevolution:end_draconium_ore'
+            'draconicevolution:end_draconium_ore',
+            // Railcraft
+            'railcraft:sulfur_ore',
+            'railcraft:deepslate_sulfur_ore',
+            'railcraft:salpeter_ore',
+            'railcraft:lead_ore',
+            'railcraft:deepslate_lead_ore',
+            'railcraft:nickel_ore',
+            'railcraft:deepslate_nickel_ore',
+            'railcraft:silver_ore',
+            'railcraft:deepslate_silver_ore',
+            'railcraft:tin_ore',
+            'railcraft:deepslate_tin_ore',
+            'railcraft:zinc_ore',
+            'railcraft:deepslate_zinc_ore',
         ];
-    });-
+    });
     console.info('Ore cleanup DONE');
 });

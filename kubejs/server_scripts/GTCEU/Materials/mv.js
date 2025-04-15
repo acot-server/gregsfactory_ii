161522b1de85e48cafc9aaab061f4ff597d86834
Annwan <annwan@annwan.me>
2025-04-15 18:27:35 +0200

    make cetane-boosted diesel obtainable in mv

--- kubejs/server_scripts/GTCEU/Materials/mv.js
+++ kubejs/server_scripts/GTCEU/Materials/mv.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(ev => {
+    // MV Casing
     ev.remove('gtceu:shaped/casing_mv');
     ev.remove('gtceu:assembler/casing_mv');
-
     ev.shaped(
         'gtceu:mv_machine_casing',
         [ 'PPP'
@@ -18,6 +18,30 @@ ServerEvents.recipes(ev => {
         .circuit(8)
         .EUt(16)
         .duration(50);
+
+    // Cetane boosted diesel (for rockets)
+    ev.remove('gtceu:mixer/cetane_diesel_from_biodiesel');
+    ev.remove('gtceu:mixer/cetane_diesel_from_diesel');
+    ev.recipes.gtceu.mixer('new_cetane_biodiesel')
+        .inputFluids(
+            Fluid.of('gtceu:bio_diesel', 1000),
+            Fluid.of('gtceu:tetranitromethane', 40)
+        )
+        .outputFluids(
+            Fluid.of('gtceu:cetane_boosted_diesel', 750)
+        )
+        .EUt(120)
+        .duration(40);
+    ev.recipes.gtceu.mixer('new_cetane_diesel')
+        .inputFluids(
+            Fluid.of('gtceu:diesel', 1000),
+            Fluid.of('gtceu:tetranitromethane', 40)
+        )
+        .outputFluids(
+            Fluid.of('gtceu:cetane_boosted_diesel', 1000)
+        )
+        .EUt(120)
+        .duration(40);
 });
 
 ServerEvents.tags('item', ev => {
